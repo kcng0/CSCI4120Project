62075386bb20b643da3fe66f1fa7adaa726c2aef
ctchan0 <1155141869@link.cuhk.edu.hk>
2022-05-18 17:29:24 +0800

    Chest Mission (get key 6)

--- Project/Assets/Inventory/scripts/OpenBox.cs
+++ Project/Assets/Inventory/scripts/OpenBox.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class OpenBox : Interactable
 {
-public Item Item;
-public Item key;
+    public Item Item;
+    public Item key;
+    public GameObject chestGuard;
     public void Pickup()
     {
         InventoryManager.Instance.Add(Item);
@@ -15,16 +16,18 @@ public Item key;
     public override string GetDescription() {
         Item item = GameObject.Find("PlayerCapsule").GetComponent<InteractItem>().EquipedItem;
         string keyName = key.itemName;
-        if (item != null && item == key && Item != null)
+        if (item != null && item == key && Item != null && chestGuard == null)
             return "Press [E] to open the box";
         else if (Item == null)
             return "This box is empty!";
+        else if (chestGuard != null)
+            return "Chest guard isn't defeated!";
         else return "You need " + keyName + " to open this box!";
     }
 
     public override void Interact() {
         Item item = GameObject.Find("PlayerCapsule").GetComponent<InteractItem>().EquipedItem;
-        if (item != null && item == key && Item != null)
+        if (item != null && item == key && Item != null && chestGuard == null)
         {
             Pickup();
         }
